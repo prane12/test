139380bc89be4bc240b36fd5796f8afc46369344
prane12 <pranesh4843@gmail.com>
2024-11-13 21:38:04 +0530

    content was not good

--- footwears.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-shoes=("nike" "adi" "sneakers" "redtape" "puma")
-HOSTNAME=$(hostname)
-echo " on this soar pune I will wear ${shoes[0]} shoes"
-
-echo " you are seeing the content from $HOSTNAME "
